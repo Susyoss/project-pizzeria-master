96ba4f7a1838e2ba9624606c1a34558fd4a10df3
Susanna Yossef <susannayossef98@gmail.com>
2022-01-29 15:25:47 +0100

    update

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,4 @@
-/* global Handlebars, dataSource */
+/* global dataSource */
 
 export const utils = {}; // eslint-disable-line no-unused-vars
 
@@ -9,7 +9,7 @@ utils.createDOMFromHTML = function(htmlString) {
 };
 
 utils.createPropIfUndefined = function(obj, key, value = []){
-  if(!obj.hasOwnProperty(key)){
+  if(!obj.hasOwnProperty(key)){ // eslint-disable-line
     obj[key] = value;
   }
 };
